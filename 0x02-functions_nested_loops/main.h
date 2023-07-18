f6f9da105414c2c3b2f88c4ce65ff12cbcb51d5d
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-18 16:28:28 -0700

    Slower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-void print_alphabet_x10(void);
 int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif /* MAIN_H */
