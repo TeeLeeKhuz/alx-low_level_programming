9c362532db9f08945165fe0c9b917842db064250
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-19 12:18:05 -0700

    PrintLastDigit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,36 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 int main(void)
 {
 int r;
-r = print_sign(98);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = print_sign(0);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = print_sign(0xff);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = print_sign(-1);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
-}
+print_last_digit(98);
+print_last_digit(0);
+r = print_last_digit(-1024);
+_putchar('0' + r);
+_putchar('\n');
+return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-int print_sign(int n);
-int _abs(int n);
+int print_last_digit(int n);
 #endif /* MAIN_H */
