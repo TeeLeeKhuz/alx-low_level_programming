0e3ce608ba03a06f8e7dc2827c2312718630e712
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-20 13:59:44 -0700

    Digit

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,20 +1,26 @@
 #include "1-main.h"
 #include <stdio.h>
+
 /**
-* main - Check the _isdigit function.
-*
-* Return: Always 0.
-*/
+ * main - Check the _isdigit function.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-char c;
-c = '0';
-printf("%c: %d\n", c, _isdigit(c));
-c = '5';
-printf("%c: %d\n", c, _isdigit(c));
-c = '9';
-printf("%c: %d\n", c, _isdigit(c));
-c = 'a';
-printf("%c: %d\n", c, _isdigit(c));
-return (0);
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+
+    c = '5';
+    printf("%c: %d\n", c, _isdigit(c));
+
+    c = '9';
+    printf("%c: %d\n", c, _isdigit(c));
+
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+
+    return (0);
 }
