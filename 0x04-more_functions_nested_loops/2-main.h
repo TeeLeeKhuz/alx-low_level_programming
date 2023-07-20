45d56a3bd0ca472c222ce3e66d9219ad58107892
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-20 14:08:40 -0700

    Mul

---
+++ 0x04-more_functions_nested_loops/2-main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _isdigit(int c);
+int mul(int a, int b);
+/* Add prototypes for other functions used in your project */
+#endif /* MAIN_H */
