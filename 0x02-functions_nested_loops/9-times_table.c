3e80a1a232a74e8a29ec540bdddfa2d04b447f8f
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-20 09:04:41 -0700

    TimesTable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,17 +33,7 @@ void times_table(void)
 
             if (col != 9)
             {
-                if ((row * (col + 1)) < 10)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
+                _putchar(' ');
             }
         }
         _putchar('\n');
