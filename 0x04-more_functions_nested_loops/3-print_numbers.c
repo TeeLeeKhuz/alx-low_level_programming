05b94ea56484c906465650791ffcc24ac27bbe6f
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-20 16:11:54 -0700

    PrintNumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_numbers - Print numbers from 0 to 9 followed by a new line.
+*/
+void print_numbers(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+_putchar(num + '0'); /* Convert the number to its corresponding character */
+}
+_putchar('\n'); /* Print a new line after printing the numbers */
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* Function to print a single character */
 int _putchar(char c);
-/* Function to check if a character is a digit */
-int _isdigit(int c);
-/* Function to multiply two integers */
-int mul(int a, int b);
-/* Add prototypes for other functions used in your project */
+void print_numbers(void);
 #endif /* MAIN_H */
