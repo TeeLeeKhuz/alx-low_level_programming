4f106ed58d54ef30c49134c2fd19fb78c855d02a
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-24 12:51:35 -0700

    PrintRev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* Function prototype for _strlen */
-int _strlen(char *s);
+/* Function prototype for _putchar */
+int _putchar(char c);
+/* Function prototype for print_rev */
+void print_rev(char *s);
 #endif /* MAIN_H */
