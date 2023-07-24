4378cdf137805e082a58379daeace2d53d8c93bc
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-24 11:01:51 -0700

    Strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,20 @@
 #include "main.h"
+
 /**
-* _strlen - Returns the length of a string.
-*
-* @s: Pointer to the string to be measured.
-* Return: The length of the string.
-*/
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to the string to be measured.
+ * Return: The length of the string.
+ */
 int _strlen(char *s)
 {
-int length = 0;
-while (*s != '\0')
-{
-length++;
-s++;
-}
-return length;
+    int length = 0;
+
+    while (*s != '\0')
+    {
+        length++;
+        s++;
+    }
+
+    return length;
 }
