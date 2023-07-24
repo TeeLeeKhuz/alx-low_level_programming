af2a56219f07a0396657f70446cfcb3cd973eaa9
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-24 13:04:32 -0700

    Puts2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,6 @@ int _putchar(char c);
 void print_rev(char *s);
 /* Function prototype for rev_string */
 void rev_string(char *s);
+/* Function prototype for puts2 */
+void puts2(char *str);
 #endif /* MAIN_H */
