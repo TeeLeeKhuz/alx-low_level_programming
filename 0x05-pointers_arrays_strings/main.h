709925d80f1e7cc4aa83d18de2738936481fa0a2
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-24 14:45:42 -0700

    Atoi

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-/* Function prototype for _putchar */
 int _putchar(char c);
-/* Function prototype for print_rev */
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
-/* Function prototype for rev_string */
 void rev_string(char *s);
-/* Function prototype for puts2 */
 void puts2(char *str);
-/* Function prototype for puts_half */
 void puts_half(char *str);
-/* Function prototype for print_array */
-void print_array(int *a, int n);
-/* Function prototype for _strcpy */
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif /* MAIN_H */
