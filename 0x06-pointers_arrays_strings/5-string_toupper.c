cfaef00160618ae0d1cdb913e9f24385462f73f4
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-26 14:10:24 -0700

    StringToupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* string_toupper - Changes all lowercase letters to uppercase.
+* @str: Pointer to the input string.
+*
+* Return: Pointer to the modified string.
+*/
+char *string_toupper(char *str)
+{
+while (*str != '\0')
+{
+if (*str >= 'a' && *str <= 'z')
+{
+*str = *str - ('a' - 'A');
+}
+str++;
+}
+return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-void reverse_array(int *a, int n);
+char *string_toupper(char *str);
 #endif /* MAIN_H */
