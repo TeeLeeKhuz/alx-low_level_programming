c6f020956daee9c015d9df5327ce125362ef9269
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-26 15:03:22 -0700

    CapString

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,33 @@
 #include "main.h"
-#include <stdbool.h>
 /**
-* is_separator - Check if a character is a word separator.
-* @ch: The character to check.
+* cap_string -  Capitalizes all words of a string
+* @str: the string to be capitalized
 *
-* Return: true if the character is a separator, false otherwise.
+* Return: A pointer to the changed string
 */
-bool is_separator(char ch)
-{
-char separators[] = " \t\n,;.!?\"(){}";
-int i;
-for (i = 0; separators[i] != '\0'; i++)
-{
-if (ch == separators[i])
-return (true);
-}
-return (false);
-}
 char *cap_string(char *str)
 {
-int i;
-if (str[0] >= 'a' && str[0] <= 'z')
-str[0] = str[0] - ('a' - 'A');
-for (i = 1; str[i] != '\0'; i++)
+int index = 0;
+while (str[index])
 {
-if (is_separator(str[i - 1]) && (str[i] >= 'a' && str[i] <= 'z'))
-str[i] = str[i] - ('a' - 'A');
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
 }
 return (str);
 }
