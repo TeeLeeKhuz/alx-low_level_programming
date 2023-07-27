b062ff5d78eb719d6679dbf17214c8ec200413f0
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-07-27 04:23:28 -0700

    PrintNumbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,24 @@
 */
 void print_number(int n)
 {
+int div = 1;
+if (n == 0)
+{
+_putchar('0');
+return;
+}
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
-if (n / 10 != 0)
+while (n / div >= 10)
 {
-print_number(n / 10);
+div *= 10;
 }
-_putchar(n % 10 + '0');
+while (div != 0)
+{
+_putchar((n / div) % 10 + '0');
+div /= 10;
+}
 }
