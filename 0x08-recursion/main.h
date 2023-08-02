3b0fb052d4e665ca1313ba03d62ebb9944ff4536
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-08-02 12:50:06 -0700

    PutsRecursin

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Function prototype for _putchar */
+int _putchar(char c);
+/* Function prototype for _puts_recursion */
+void _puts_recursion(char *s);
+#endif /* MAIN_H */
