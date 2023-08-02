00678e1042fdfdac426611fad23a8dc6ed12e5f4
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-08-02 13:54:14 -0700

    SqrtRecursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,13 +8,13 @@
 */
 int find_sqrt(int n, int guess)
 {
-if (guess * guess == n) {
+if (guess * guess == n)
 return (guess); /* Base case: Square root found. */
-}
-if (guess * guess > n) {
+
+if (guess * guess > n)
 return (-1); /* Square root is not a natural number. */
-}
-/* Recursively increment the guess until the square root is found or not a natural number. */
+
+/* Recursively increment square root is found or not a natural number. */
 return (find_sqrt(n, guess + 1));
 }
 /**
@@ -25,9 +25,8 @@ return (find_sqrt(n, guess + 1));
 */
 int _sqrt_recursion(int n)
 {
-if (n < 0) {
-return (-1); /* Error condition: Negative numbers don't have a natural square root. */
-}
+if (n < 0)
+return (-1); /* Negative numbers don't have a natural square root */
 /* Start finding the square root with a guess of 1. */
 return (find_sqrt(n, 1));
 }
