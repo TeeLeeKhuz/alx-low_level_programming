f5b7fff9cbc35f7101b4fa31a1273828be63c4b6
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-08-02 13:06:56 -0700

    PrintRevRecursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,8 +5,8 @@
 */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0') /* Base case: If we reach the end of the string (null terminator), return. */
+if (*s == '\0') /* Reach the end of the string (null terminator), return */
 return;
-_print_rev_recursion(s + 1); /* Recursively call the function with the next character in the string. */
-_putchar(*s); /* Print the current character after all the recursive calls have been executed. */
+_print_rev_recursion(s + 1); /* Recursively call the function in the string */
+_putchar(*s); /* Print character after recursive calls have been executed */
 }
