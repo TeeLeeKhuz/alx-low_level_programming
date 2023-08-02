ad659786fb81952c9ca62d83cbe7e89dd5bf0bd3
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-08-02 13:13:46 -0700

    StrlenRecursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,4 +4,6 @@
 int _putchar(char c);
 /* Function prototype for _print_rev_recursion */
 void _print_rev_recursion(char *s);
+/* Function prototype for _strlen_recursion */
+int _strlen_recursion(char *s);
 #endif /* MAIN_H */
