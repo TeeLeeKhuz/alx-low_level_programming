e285a4f3dfca2c89c35ba2e6c1ac1ed06f813733
TeeLeeKhuz <TeeLeeKhuz@gmail.com>
2023-08-09 03:29:54 -0700

    WhatsMyName

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Prototype for the _putchar function */
+int _putchar(char c);
+/* Add any other function prototypes your project requires below */
+#endif /* MAIN_H */
